0b17ed517bef99fb1522057bb10cc3e27671b82a
emuthombene <emuthombene@gmail.com>
2017-05-23 00:27:28 +0200

    voltando ao inicio

--- src/com/clinica/bean/PacienteBean.java
+++ src/com/clinica/bean/PacienteBean.java
@@ -2,12 +2,6 @@ package com.clinica.bean;
 
 public class PacienteBean {
 
-  public static void main(String[] args){
-    System.out.printla("Testando actualizacao Exportando modificacoes do GitHUb");
-  }
   
-  public void setTEste(){
-	  
-  }
 
 }
