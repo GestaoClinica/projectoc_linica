89ecbbfa6ba863de31f4dd972ecad9336cd80169
emuthombene <emuthombene@gmail.com>
2017-05-25 19:02:10 +0200

    importe corigido

--- src/Clinica/conexao/Conexao.java
+++ src/Clinica/conexao/Conexao.java
@@ -1,31 +1,27 @@
 package Clinica.conexao;
 
+
+import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import java.sql.Connection;
-
 public class Conexao {
-private static final String USUARIO="root";
-private static final String SENHA="1846";
-private static final String URL="jdbc:mysql://localhost:3306/Clinica";
+	private static final String USUARIO = "root";
+	private static final String SENHA = "root";
+	private static final String URL = "jdbc:mysql://localhost:3306/Clinica";
 
-
-public static Connection conectar() throws SQLException {
-	
-Connection conexao=DriverManager.getConnection(URL, USUARIO, SENHA);
-return conexao;
-}
-
-
-public static void main(String[] args) {
-  try {
-      Connection conexao= Conexao.conectar(); 
-      System.out.println("conexao realizada com sucesso");
-  } catch (SQLException ex) {
-      System.out.println("nao foi possivel  realizar a conexao" );
-  }
-   
-}
-
+	public static Connection conectar() throws SQLException{
+		Connection conexao = DriverManager.getConnection(URL, USUARIO, SENHA);
+		return conexao;
+	}
+	public static void main(String[] args) {
+		try{
+		Connection conexao = Conexao.conectar();
+			System.out.println("Conexao realizada com sucesso");
+		} catch (SQLException ex){
+			//ex.printStackTrace();
+			System.out.println("Nao foi possivel realizar a conexao");
+		}
+	}
+		
 }
